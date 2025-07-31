660c622cb047cffea9b602083ad43dac31bf41bf
famonterrey13 <famonterrey@uc.cl>
2025-07-30 21:18:41 -0400

    Se agrega función para encontrar producto en el catálogo

--- 6.catalogo/catalogo.js
+++ 6.catalogo/catalogo.js
@@ -15,13 +15,23 @@ const eliminarProducto = function(nombre){
     }
 }
 
+const buscarProducto = function(nombre){
+    const index = catalogo.findIndex(catalogo => catalogo.nombre === nombre);
+    if (index !== -1){
+        console.log("Producto " + nombre + " encontrado");
+        console.log(catalogo[index])
+    } else {
+        console.log("Producto " + nombre + " no encontrado")
+    }
+}; 
+
 function mostarCatalogo(){
     let nombre = prompt("ingresa nombre").toLowerCase(); //Recuerda siempre agregar () para toLowerCase
     let precio = parseInt(prompt("ingresa precio"));
     agregarProducto(nombre,precio);
     console.log(catalogo);
     let nombreEliminar = prompt("ingresa el nombre del producto a eliminar").toLowerCase();
-    eliminarProducto(nombreEliminar);
+    buscarProducto(nombreEliminar);
     console.log(catalogo);
 }
 
