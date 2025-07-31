29123c426a3b0428240ac6bc7cf880df7b1d4378
famonterrey13 <famonterrey@uc.cl>
2025-07-30 20:33:02 -0400

    Se agrega función para eliminar producto

--- 6.catalogo/catalogo.js
+++ 6.catalogo/catalogo.js
@@ -5,10 +5,24 @@ function agregarProducto(nombre,precio){ //2Función de declaración con la que 
     console.log("Producto " + nombre + " agregado");
 }
 
+const eliminarProducto = function(nombre){
+    const index = catalogo.findIndex(catalogo => catalogo.nombre === nombre);
+    if (index !== -1){
+        catalogo.splice(index,1);
+        console.log("Producto " + nombre + " eliminada");
+    } else {
+        console.log("Producto " + nombre + " no encontrado");
+    }
+}
+
 function mostarCatalogo(){
     let nombre = prompt("ingresa nombre").toLowerCase(); //Recuerda siempre agregar () para toLowerCase
     let precio = parseInt(prompt("ingresa precio"));
     agregarProducto(nombre,precio);
+    console.log(catalogo);
+    let nombreEliminar = prompt("ingresa el nombre del producto a eliminar").toLowerCase();
+    eliminarProducto(nombreEliminar);
+    console.log(catalogo);
 }
 
 mostarCatalogo();
