024bd5b2067ddcb67d3eb89391ed4efd4ee9f811
famonterrey13 <famonterrey@uc.cl>
2025-07-30 22:24:10 -0400

    Se agrega función menu que permite iterar el resto de las funciones

--- 6.catalogo/catalogo.js
+++ 6.catalogo/catalogo.js
@@ -13,26 +13,57 @@ const eliminarProducto = function(nombre){
     } else {
         console.log("Producto " + nombre + " no encontrado");
     }
-}
+};
 
 const buscarProducto = function(nombre){
     const index = catalogo.findIndex(catalogo => catalogo.nombre === nombre);
     if (index !== -1){
         console.log("Producto " + nombre + " encontrado");
-        console.log(catalogo[index])
     } else {
         console.log("Producto " + nombre + " no encontrado")
     }
 }; 
 
-function mostarCatalogo(){
-    let nombre = prompt("ingresa nombre").toLowerCase(); //Recuerda siempre agregar () para toLowerCase
-    let precio = parseInt(prompt("ingresa precio"));
-    agregarProducto(nombre,precio);
-    console.log(catalogo);
-    let nombreEliminar = prompt("ingresa el nombre del producto a eliminar").toLowerCase();
-    buscarProducto(nombreEliminar);
-    console.log(catalogo);
+const mostrarProducto = () => {
+    console.log("Productos en el catálogo:");
+    catalogo.forEach(catalogo => {
+        console.log(`- Nombre: ${catalogo.nombre} | Precio: ${catalogo.precio} `);
+    });
+};
+
+
+function menu(){
+    let aux = true;
+    while(aux === true){
+        let opcion = prompt("¿Qué deseas hacer? ¿Agregar, Eliminar, Buscar, Mostrar'");
+        switch(opcion){
+            case "agregar":
+                let nombre = prompt("ingresa nombre").toLowerCase(); //Recuerda siempre agregar () para toLowerCase
+                let precio = parseInt(prompt("ingresa precio"));
+                agregarProducto(nombre,precio);
+                break;
+            case "eliminar":
+                let nombreEliminar = prompt("ingresa el nombre del producto a eliminar").toLowerCase();
+                eliminarProducto(nombreEliminar);
+                break;
+            case "buscar":
+                let nombreBuscar = prompt("ingresa el nombre del producto a buscar").toLowerCase();
+                buscarProducto(nombreBuscar);
+                break;
+            case "mostrar":
+                mostrarProducto();
+                break;
+            default:
+                alert("Opción no existe");
+        }
+
+        let continuar = prompt("¿Deseas hacer algo más? Si o No").toLowerCase();
+        if (continuar === "si"){
+            aux = true;
+        } else {
+            aux = false;
+        }
+    }    
 }
 
-mostarCatalogo();
+menu();
