3b0b5bcb567e7e3c4abbd6c6798b02b049835670
famonterrey13 <famonterrey@uc.cl>
2025-09-06 09:46:20 -0400

    Se agrega archivo html y js para comenzar a trabajar

---
+++ 8.ventas/ventas.js
@@ -0,0 +1,25 @@
+const ventas = [
+  {
+    cliente: 'Juan',
+    productos: [
+      { nombre: 'smartphone', cantidad: 1, precio: 350 },
+      { nombre: 'laptop', cantidad: 1, precio: 800 },
+    ],
+  },
+  {
+    cliente: 'Ana',
+    productos: [
+      { nombre: 'smartphone', cantidad: 2, precio: 350 },
+      { nombre: 'cámara', cantidad: 1, precio: 250 },
+    ],
+  },
+  {
+    cliente: 'Pedro',
+    productos: [
+      { nombre: 'laptop', cantidad: 1, precio: 800 },
+      { nombre: 'cámara', cantidad: 2, precio: 250 },
+    ],
+  },
+];
+
+// Escribe tu código aquí
